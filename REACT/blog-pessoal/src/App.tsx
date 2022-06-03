310156d026c1917eb92bcf5292e39ba921134d8b
Erick Chiappone <erick_braga_11@hotmail.com>
2022-06-03 13:21:26 -0300

    @echiappone [Task: CRIA em app.tsx Router, Route, Routes] {03/06/2022 13:21}

--- REACT/blog-pessoal/src/App.tsx
+++ REACT/blog-pessoal/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from "./paginas/home/Home"
+
+
 import './App.css';
 
-
 function App() {
-  return (
-    <>
+  return(
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/postagens" element={<Home />} />
+
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
