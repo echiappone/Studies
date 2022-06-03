a7d1b9af4897fe745d08043ac706799d228d3d35
Erick Chiappone <erick_braga_11@hotmail.com>
2022-06-03 15:32:29 -0300

    @echiappone [Task: CRIA em app.tsx Route para CadastroUsuario] {03/06/2022 15:32}

--- REACT/blog-pessoal/src/App.tsx
+++ REACT/blog-pessoal/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from "./paginas/home/Home"
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/postagens" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
         </Routes>
       </div>
