f06667502981a52716838329d8721778b54f0241
Erick Chiappone <erick_braga_11@hotmail.com>
2022-06-03 16:00:41 -0300

    @echiappone [Task: CORRIGE Service login] {03/06/2022 16:00}

--- REACT/blog-pessoal/src/services/Service.ts
+++ REACT/blog-pessoal/src/services/Service.ts
@@ -4,7 +4,8 @@ export const api = axios.create({
     baseURL: "'https://blogdotnetgen.herokuapp.com/'",
 })
 
-export const login = async (url, dados, setDado) => {
+export const login = async (url: any, dados: any, setToken: any, setId: any) => {
     const resposta = await api.post(url, dados)
-    setDado(resposta.data)
+    setToken(resposta.data.token)
+    setId(resposta.data.usuario.id)
 }
