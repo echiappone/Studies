316d5cf1e025fa50dfcfa976cad0dbfe394c7d4b
Erick Chiappone <erick_braga_11@hotmail.com>
2022-06-03 15:34:02 -0300

    @echiappone [Task: MODIFICA pagina Login] {03/06/2022 15:34}

--- REACT/blog-pessoal/src/paginas/login/Login.tsx
+++ REACT/blog-pessoal/src/paginas/login/Login.tsx
@@ -12,7 +12,7 @@ function Login() {
                     <form>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
                         <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
@@ -25,7 +25,13 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>
+                                Cadastre-se
+                            </Typography>
+                        </Link>
+                        
+                        
                     </Box>
                 </Box>
             </Grid>
